6055534e8e6fea865f679d47527a7257fde64140
cxmacnew <32408300@qq.com>
2020-01-09 17:19:54 +0800

    no message

--- ios/Plugin/Plugin.h
+++ ios/Plugin/Plugin.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
-
+#import "WXApi.h"
+#import "WechatAuthSDK.h"
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
 
@@ -7,4 +8,8 @@ FOUNDATION_EXPORT double PluginVersionNumber;
 FOUNDATION_EXPORT const unsigned char PluginVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Plugin/PublicHeader.h>
+//#import "../Pods/Headers/Public/WechatOpenSDK/WXApi.h"
+//#import "/Users/lxlx-cx/BJMJ/capacitor-plugin-wechat/ios/Pods/Headers/Public/WechatOpenSDK/WXApi.h"
+
+
 
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Capacitor
 //import openSDK
-import WechatKit
+//import WechatKit
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -19,22 +19,7 @@ public class wechat: CAPPlugin {
     }
     
     @objc func loginToWx(_ call: CAPPluginCall) {
-        if WechatManager.shared.isInstalled(){
-            print("yes");
-        }else{
-            print("no");
-        }
-       WechatManager.appid = "wx7cc21f33633fa22f"
-       WechatManager.appSecret = "xxxxx"
-
-        WechatManager.shared.getUserInfo { result in
-      switch result {
-      case .failure(let errCode)://获取失败
-          print(errCode)
-      case .success(let value)://获取成功 value为([String: String]) 微信用户基本信息
-          print(value) //当前是在子线程，如需回到主线程调用 DispatchQueue.main.async { print(value) }
-      }
-  }
+        WXApi.isWXAppInstalled()
         
         
         //WXApi.registerApp("wx7cc21f33633fa22f",universalLink:"https://agent.beijingmj.com/lexun-launchvideo1plus"); //改成你实际的AppID
